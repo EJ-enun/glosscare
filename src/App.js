752431b61fe3defdba89f8293132b197aada10de
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2022-07-21 06:52:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -97,8 +97,7 @@ function App(props, { signOut }) {
       
     >
       {(item, index) => (
-       <ThemeProvider theme={theme} colorMode="light">
-      <Flex direction="row">
+       
       <Button variation="primary" grow="1" key={index}>
           <div>{item.name}</div><br/><Icon
       ariaLabel="Camera"
@@ -106,8 +105,7 @@ function App(props, { signOut }) {
       pathData="M10 8v8l5-4-5-4zm9-5H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14z"
  
     /></Button>
-    </Flex>
-    </ThemeProvider>
+    
       )}
       
     </Collection>
